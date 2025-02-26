0eab16aaa936beb6c2104cf9b76cfd2f228aadc2
broskis-69 <techtrackaffiliates@gmail.com>
2025-02-26 23:56:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+   const navMenu = document.getElementById('nav-menu')
+   // When we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SHOW DROPDOWN ===============*/
+const showDropdown = (dropdownId) =>{
+   const dropdown = document.getElementById(dropdownId)
+
+   dropdown.addEventListener('click', ()=>{
+      /* Show dropdown */
+      dropdown.classList.toggle('show-dropdown')
+   })
+}
+showDropdown('dropdown')
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+   "#ffb56b",
+   "#fdaf69",
+   "#f89d63",
+   "#f59761",
+   "#ef865e",
+   "#ec805d",
+   "#e36e5c",
+   "#df685c",
+   "#d5585c",
+   "#d1525c",
+   "#c5415d",
+   "#c03b5d",
+   "#b22c5e",
+   "#ac265e",
+   "#9c155f",
+   "#950f5f",
+   "#830060",
+   "#7c0060",
+   "#680060",
+   "#60005f",
+   "#48005f",
+   "#3d005e"
+ ];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
